760c4e8027ceb4607640a7ccebaff4bff0fd304c
IdelSaid <idelsaid@gmail.com>
2019-11-08 20:17:40 -0600

    not working

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,6 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  
+
   def new
     @gram = Gram.new
   end
@@ -9,11 +9,11 @@ class GramsController < ApplicationController
   end
 
   def create
-  @gram = current_user.grams.create(gram_params)
-  if @gram.valid?
-    redirect_to root_path
-  else
-    render :new, status: :unprocessable_entity
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
